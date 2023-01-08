b23971ccea272b3e2b07df7e87bab82b6f883801
Paul Kotsko <118366151+Pau1Net@users.noreply.github.com>
2023-01-08 14:40:31 +0300

    Create visual.py

---
+++ visual.py
@@ -0,0 +1,25 @@
+import pyglet
+
+image = pyglet.image.load('igcboard.jpg') #Test image
+
+window = pyglet.window.Window()
+
+sprite = pyglet.sprite.Sprite(image)
+
+# @window.event
+# def on_
+
+def update_image(dt):
+  window.clear()
+  sprite.draw()
+  draw_arrow()
+
+
+def draw_arrow():
+  # TODO: Implement code to draw the arrow.
+
+
+pyglet.clock.schedule_interval(update_image, 0.5) #Update time
+
+
+pyglet.app.run()
